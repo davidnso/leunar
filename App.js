3befcf2db600a58e6acc1a808d2d371542e5a428
Gustavus Shaw II <gshaw1997@gmail.com>
2019-03-30 04:06:30 -0400

    :sparkles: Implement app bootstrap and nav

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Font} from 'expo';
+import { Colors } from './Colors';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import HomeScreen from './Home/Home'
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen
+},{headerMode: 'none'});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+   await Font.loadAsync({
+      'dosis-light': require('./assets/fonts/Dosis-Light.ttf'),
+      'dosis-medium': require('./assets/fonts/Dosis-Medium.ttf'),
+      'dosis-semi-bold': require('./assets/fonts/Dosis-SemiBold.ttf'),
+      'dosis-bold': require('./assets/fonts/Dosis-Bold.ttf'),
+    });
+    this.setState({fontLoaded: true})
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    if(this.state.fontLoaded){
+      return (
+      <AppContainer></AppContainer>
+      )
+    }else{
+      return (<View style={styles.container}></View>)
+    }
+   
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.MAIN_BG,
   },
 });
+
+
