5a71339dfa951ea7fe8d1025281acbc5fe3847ef
dnsoes1 <davidnsoesie1@gmail.com>
2019-03-30 08:16:36 -0400

    add  sleep score gif to home page

--- Home/Home.js
+++ Home/Home.js
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity, Image} from 'react-native';
 import {styles} from './Home.styles'
 import StatBar from './components/stat-bar/StatBar';
 import OptionButton from './components/option-button/OptionButton';
+import SleepScore from './components/Spinner/Spinner'
 
 export default class HomeScreen extends React.Component {
     render() {
@@ -15,8 +16,8 @@ export default class HomeScreen extends React.Component {
         <View style={styles.sleepScoreSection}>
         <Image
         style={{width: 200, height: 200, marginBottom: 25}}
-          source={require('../assets/images/sleep-score.png')}
-        />
+         source={{uri: "https://media.giphy.com/media/3LdSb8AcFc6S7eQCA3/giphy.gif"}}
+        /><Text>we here wit</Text>
           <TouchableOpacity activeOpacity={0.5}  style={styles.seeMoreBtn}>
             <Text style={styles.seeMoreBtnTxt}>See More</Text>
           </TouchableOpacity>
