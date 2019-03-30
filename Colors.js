dc25e93d9418c8f79c4311bf9450fd89955ab59d
Gustavus Shaw II <gshaw1997@gmail.com>
2019-03-30 04:04:05 -0400

    :sparkles: Add colors const

---
+++ Colors.js
@@ -0,0 +1,15 @@
+export const Colors = {
+    MAIN_BG: '#110D25',
+    SPECTRUM_GRADIENT: { 
+        purple: "#E20DE9", 
+        blue: "#56EFFF",
+        green: "#0CFE91"},
+    CIRCLE_OUTLINE: { 
+        light_purple:"#BA2EC3", 
+        dark_purple:"#8430E5",
+    },
+    LUENAR_PURPLE: "#7303C0", 
+    WHITE: "#FFFF",
+    SOFT_WHITE: "#E8E6EB",
+    BLACK: '#000000'
+}
